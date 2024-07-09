67d3c88a2435e3bb4f8842f4c06d4bc483cfb311
Mehdi <mehdi@molekulce.com>
2024-07-09 10:46:56 +0300

    Updated dgl

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DEST_DIR="/home/$USER/.conda/envs/RNN-MD-2/lib/python3.6/site-packages"
+
+
 # Create and activate conda environment
 conda create -n RNN-MD python=3.6 numpy -y
 source activate RNN-MD
@@ -7,6 +10,9 @@ source activate RNN-MD
 pip install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 conda install -c dglteam dgl-cuda10.1==0.5* -y
 conda install cudatoolkit=10.1 -y
+mv dgl "$DEST_DIR"
+mv dgl-0.5.0-py3.6.egg-info "$DEST_DIR"
+ 
 
 # Install Python dependencies
 pip install -r requirements.txt
